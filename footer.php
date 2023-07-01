69ed773962a8e1feb383d1ade1e847f127e6fa94
romanZhurakivskyi <zhurakivskyy19@gmail.com>
2023-07-01 16:43:11 +0300

    mini-blog

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+		   
+            <p class="copyright"><a href="https://youtube.com">Follow</a></p>
+
+            <?php 
+                dynamic_sidebar('footer-1');
+            ?>  
+		   
+	    </footer>
+    
+    </div>
+    
+	<?php
+		wp_footer();
+	?>
+           
+
+</body>
+</html> 
